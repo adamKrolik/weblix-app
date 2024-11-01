663e7c45c5b110e6a2b46fd6a80c94e75634f0fc
Adam Krolik <krolikadam9@gmail.com>
2024-11-01 12:49:20 +0100

    Added swiper

--- script.js
+++ script.js
@@ -18,6 +18,53 @@ async function fetchAPIData(endpoint) {
   
 }
 
+async function displaySlider() {
+  const { results } = await fetchAPIData('movie/now_playing');
+  
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide');
+    div.innerHTML = `      
+        <a href="movie-details.html?id=${movie.id}">
+          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+        </a>
+        <h4 class="swiper-rating">
+          <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(1)} / 10
+        </h4>      
+    `;
+
+    document.querySelector('.swiper-wrapper').appendChild(div);
+
+    
+  });
+  initSwiper();
+  
+}
+
+function initSwiper() {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    }
+  });
+}
+
 function displayBackdropImage(backdrop) {
   const container = document.querySelector('.flex-container');
   
@@ -40,8 +87,6 @@ function displayBackdropImage(backdrop) {
 
 async function displayPopularMovies() {
   const data = await fetchAPIData('movie/popular');
-
-  console.log(data.results);
 
   data.results.forEach((movie) => {
     const div = document.createElement('div');
@@ -250,6 +295,7 @@ function init() {
   switch(global.currentPage) {
     case "/":
     case "/index.html":
+      displaySlider()
       displayPopularMovies();
       break;
 
